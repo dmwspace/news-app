f22cc76d963701283c9644a98be7f3451bf1d2e1
dean <dean@deans-iMac.home>
2022-03-24 23:56:37 -0700

    Added useEffect

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import Header from './components/Header';
 
 function App() {
 
+  useEffect(() => {
+    const apiKey = process.env.API_KEY
+    const options = {
+      method: "GET",
+      headers: {
+        'X-RapidAPI-Host': 'free-news.p.rapidapi.com',
+        'X-RapidAPI-Key': apiKey
+      }
+    }
+    fetch('https://free-news.p.rapidapi.com/v1/search?q=Headlines&lang=en', options)
+      .then(res => res.json())
+      .then(res => console.log(res))
+      .catch(e => console.error(e))
+  }, [])
+
   return (
     <div>
       <Header />
